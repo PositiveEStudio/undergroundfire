d396c73e2a054a9623d6a51372cee8f3b1d85d70
mcxufly <mcxufly@outlook.com>
2023-07-08 00:34:31 +0800

    删除无用行

--- src/main/java/dev/positivee/undergroundfire/UndergroundFire.java
+++ src/main/java/dev/positivee/undergroundfire/UndergroundFire.java
@@ -8,8 +8,6 @@ import dev.positivee.undergroundfire.item.ItemRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -23,22 +21,11 @@ public class UndergroundFire
 	{
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
-		bus.addListener(this::setup);
-		bus.addListener(this::clientSetup);
-
 		MinecraftForge.EVENT_BUS.register(this);
 
 		ItemRegistry.ITEMS.register(bus);
 		BlockRegistry.BLOCKS.register(bus);
 		BlockEntityRegistry.BLOCK_ENTITY_TYPE.register(bus);
 		CreativeTabRegistry.CREATIVE_MODE_TAB.register(bus);
-	}
-
-	public void clientSetup(FMLClientSetupEvent event)
-	{
-	}
-
-	private void setup(final FMLCommonSetupEvent event)
-	{
 	}
 }
