e1c21239d10e6aea58e86c893d668414f5d5ce1e
mcxufly <mcxufly@outlook.com>
2023-07-24 02:24:03 +0800

    logger

--- src/main/java/dev/positivee/undergroundfire/UndergroundFire.java
+++ src/main/java/dev/positivee/undergroundfire/UndergroundFire.java
@@ -1,5 +1,6 @@
 package dev.positivee.undergroundfire;
 
+import com.mojang.logging.LogUtils;
 import dev.positivee.undergroundfire.block.BlockRegistry;
 import dev.positivee.undergroundfire.block.entity.BlockEntityRegistry;
 import dev.positivee.undergroundfire.creative.CreativeTabRegistry;
@@ -9,11 +10,13 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 
 @Mod(UndergroundFire.MODID)
 public class UndergroundFire
 {
 	public static final String MODID = "undergroundfire";
+	private static final Logger LOGGER =LogUtils.getLogger();
 	public static final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
 	public UndergroundFire()
