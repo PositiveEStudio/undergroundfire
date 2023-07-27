237b1be3b0c5b3e94840463029ccae71498a495a
mcxufly <mcxufly@outlook.com>
2023-07-27 14:57:58 +0800

    气化煤提取器材质

--- src/main/java/dev/positivee/undergroundfire/block/BlockGasCoalExtractor.java
+++ src/main/java/dev/positivee/undergroundfire/block/BlockGasCoalExtractor.java
@@ -4,6 +4,9 @@ import dev.positivee.undergroundfire.block.entity.BlockEntityGasCoalExtractor;
 import dev.positivee.undergroundfire.block.entity.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
@@ -13,19 +16,31 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class BlockGasCoalExtractor extends BaseEntityBlock
 {
 	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+	public static final BooleanProperty TANK = BooleanProperty.create("tank");
 
 	public BlockGasCoalExtractor()
 	{
 		super(Properties.of().mapColor(MapColor.METAL).requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL));
-		this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH));
+		this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH).setValue(TANK, false));
+	}
+
+	@Override
+	public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit)
+	{
+//		int gasCoal = pLevel.getBlockEntity(pPos, BlockEntityRegistry.GAS_COAL_EXTRACTOR.get()).get().getGasCoal();
+//		pPlayer.sendSystemMessage(Component.translatable(String.valueOf(gasCoal)));
+
+		return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
 	}
 
 	@Nullable
@@ -44,7 +59,7 @@ public class BlockGasCoalExtractor extends BaseEntityBlock
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder)
 	{
-		pBuilder.add(FACING);
+		pBuilder.add(FACING).add(TANK);
 	}
 
 	@Nullable
--- src/main/java/dev/positivee/undergroundfire/block/entity/BlockEntityGasCoalExtractor.java
+++ src/main/java/dev/positivee/undergroundfire/block/entity/BlockEntityGasCoalExtractor.java
@@ -1,11 +1,18 @@
 package dev.positivee.undergroundfire.block.entity;
 
+import dev.positivee.undergroundfire.block.BlockRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+import static dev.positivee.undergroundfire.block.BlockGasCoalExtractor.FACING;
 
 public class BlockEntityGasCoalExtractor extends BlockEntity
 {
@@ -18,49 +25,44 @@ public class BlockEntityGasCoalExtractor extends BlockEntity
 
 	public static void tick(Level level, BlockPos pos, BlockState state, BlockEntityGasCoalExtractor blockEntity)
 	{
-//		ArrayList<BlockPos> targetPos = new ArrayList<>();
-//
-//		for (int i = -1; i <= 1; i++)
-//		{
-//			for (int j = -1; j <= 1; j++)
-//			{
-//				for (int k = -1; k <= 1; k++)
-//				{
-//					if (i == 0 && j == 0 && k == 0) continue;
-//
-//					BlockPos newPos = new BlockPos(pos.getX() + i, pos.getY() + j, pos.getZ() + k);
-//					Block block = level.getBlockState(newPos).getBlock();
-//					if (block.equals(Blocks.AIR) || block.equals(BlockRegistry.GAS_COAL.get()))
-//					{
-//						targetPos.add(newPos);
-//						if (level.isClientSide)
-//							level.addParticle(DustParticleOptions.REDSTONE, true, newPos.getX(), newPos.getY(), newPos.getZ(), 0.0D, 0.0D, 0.0D);
-//					}
-//				}
-//			}
-//		}
-//
-//		if (!targetPos.isEmpty())
-//		{
-//			int index;
-//
-//			if (targetPos.size() == 1)
-//				index = 0;
-//			else
-//				index = level.random.nextInt(0, targetPos.size() - 1);
-//
-//			BlockState targetState = level.getBlockState(targetPos.get(index));
-//			Block targetBlock = targetState.getBlock();
-//
-//			if (!level.isClientSide)
-//			{
-//				int targetConcentration = targetState.getValue(BlockGasCoal.CONCENTRATION);
-//
-//				level.setBlock(targetPos.get(index), targetState.setValue(BlockGasCoal.CONCENTRATION, targetConcentration + 1), 2);
-//				level.getBlockEntity(targetPos.get(index), BlockEntityRegistry.GAS_COAL.get()).get().isDelay();
-//				level.setBlock(pos, blockEntity.getBlockState().setValue(BlockGasCoal.CONCENTRATION, (concentration >= 1) ? (concentration - 1) : 0), 2);
-//			}
-//		}
+		ArrayList<BlockPos> targetPos = new ArrayList<>();
+		Direction direction = state.getValue(FACING);
+
+		for (int i = -1; i <= 1; i++)
+		{
+			for (int j = -1; j <= 1; j++)
+			{
+				for (int k = -1; k <= 1; k++)
+				{
+					if (i == 0 && j == 0 && k == 0) continue;
+
+					BlockPos newPos = new BlockPos(pos.getX() + i, pos.getY() + j, pos.getZ() + k);
+					Block block = level.getBlockState(newPos).getBlock();
+					if (block.equals(BlockRegistry.GAS_COAL.get()))
+					{
+						targetPos.add(newPos);
+					}
+				}
+			}
+		}
+
+		if (!targetPos.isEmpty())
+		{
+			int index;
+
+			if (targetPos.size() == 1)
+				index = 0;
+			else
+				index = level.random.nextInt(0, targetPos.size() - 1);
+
+			BlockState targetState = level.getBlockState(targetPos.get(index));
+			Block targetBlock = targetState.getBlock();
+
+			if (!level.isClientSide)
+			{
+
+			}
+		}
 	}
 
 	@Override
@@ -93,5 +95,10 @@ public class BlockEntityGasCoalExtractor extends BlockEntity
 		// Client side, read NBT when updating chunk data
 		super.load(tag);
 		this.gasCoal = tag.getInt("GasCoal");
+	}
+
+	public int getGasCoal()
+	{
+		return this.gasCoal;
 	}
 }
