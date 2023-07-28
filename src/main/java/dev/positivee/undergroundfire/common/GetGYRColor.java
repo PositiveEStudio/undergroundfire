373f5ea90ab00d2b2f90ffd3848b15d43961df7b
mcxufly <mcxufly@outlook.com>
2023-07-28 17:06:15 +0800

    常数

--- src/main/java/dev/positivee/undergroundfire/common/GetGYRColor.java
+++ src/main/java/dev/positivee/undergroundfire/common/GetGYRColor.java
@@ -8,22 +8,24 @@ public class GetGYRColor
 	public static Vector3f getParticleColor(int targetConc)
 	{
 		int red, green;
+		double half = (double) Constants.CONC_MAX / 2.0D;
 		String color_s;
 
-		if (targetConc < 50)
+
+		if (targetConc < half)
 		{
-			red = (int) (255 * ((double) targetConc / 50.0D));
+			red = (int) (255 * ((double) targetConc / half));
 			color_s = Integer.toHexString(red) + "ff00";
 		}
 
-		else if (targetConc == 50)
+		else if (targetConc == half)
 		{
 			color_s = "ffff00";
 		}
 
 		else
 		{
-			green = 255 - (int) (255 * (((double) targetConc - 50.00D) / 50.0D));
+			green = 255 - (int) (255 * ((double) targetConc - half) / half);
 			color_s = "ff" + Integer.toHexString(green) + "00";
 		}
 
