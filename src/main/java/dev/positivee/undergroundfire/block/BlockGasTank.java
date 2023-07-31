ef825797c9e36dbf44d8da342896cc76c1998a52
mcxufly <mcxufly@outlook.com>
2023-08-01 01:15:35 +0800

    煤气罐掉落物和方块tolerance匹配

--- src/main/java/dev/positivee/undergroundfire/block/BlockGasTank.java
+++ src/main/java/dev/positivee/undergroundfire/block/BlockGasTank.java
@@ -1,9 +1,12 @@
 package dev.positivee.undergroundfire.block;
 
+import dev.positivee.undergroundfire.item.ItemRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -11,11 +14,16 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlockGasTank extends Block
 {
@@ -37,6 +45,31 @@ public class BlockGasTank extends Block
 	public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit)
 	{//TODO:在冒烟位置生成实体释放粒子效果，并发出点燃的声音，延时一定时间后爆炸，根据 tolerance 值去设定爆炸等级，tolerance 值为 0 时不触发
 		return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+	}
+
+	@Override
+	public List<ItemStack> getDrops(BlockState pState, LootParams.Builder pParams)
+	{
+		List<ItemStack> drops = new ArrayList<>();
+		ItemStack drop = new ItemStack(ItemRegistry.GAS_TANK.get(), 1);
+//		drop.addTagElement("tolerance", IntTag.valueOf(pState.getValue(TOLERANCE)));
+		drop.getOrCreateTag().putInt("tolerance", pState.getValue(TOLERANCE));
+		drops.add(drop);
+		return drops;
+	}
+
+	@Override
+	public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, @Nullable LivingEntity pPlacer, ItemStack pStack)
+	{
+		if (pStack.getOrCreateTag().contains("tolerance"))
+		{
+			pLevel.setBlockAndUpdate(pPos,pState.setValue(TOLERANCE, pStack.getOrCreateTag().getInt("tolerance")));
+		}
+		else
+		{
+			pState.setValue(TOLERANCE, 0);
+		}
+		super.setPlacedBy(pLevel, pPos, pState, pPlacer, pStack);
 	}
 
 	@Override
